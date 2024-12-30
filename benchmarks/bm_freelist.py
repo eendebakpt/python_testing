154907ed1f33dd1b2e01484a154950eee4620091
Pieter Eendebak <pieter.eendebak@gmail.com>
2024-12-30 23:27:17 +0100

    bm for freelists

---
+++ benchmarks/bm_freelist.py
@@ -0,0 +1,110 @@
+# Quick benchmark for cpython freelists
+
+import pyperf
+
+
+def bench_list(loops):
+    range_it = iter(range(loops))
+    tpl = tuple(range(100))
+
+    t0 = pyperf.perf_counter()
+    for ii in range_it:
+        for ii in tpl:
+            _ = [ii]
+            _ = [ii, ii + 1]
+            _ = [ii, ii + 1, ii]
+    return pyperf.perf_counter() - t0
+
+
+def collatz(a):
+    while a > 1:
+        if a % 2 == 0:
+            a = a // 2
+        else:
+            a = 3 * a + 1
+
+
+def bench_int(loops):
+    range_it = range(loops)
+    tpl = tuple(range(200, 300))
+
+    t0 = pyperf.perf_counter()
+    for ii in range_it:
+        for jj in tpl:
+            collatz(jj)
+    return pyperf.perf_counter() - t0
+
+
+def bench_float(loops):
+    range_it = iter(range(loops))
+    tpl = tuple(range(500))
+
+    t0 = pyperf.perf_counter()
+    for ii in range_it:
+        x = 0
+        for ii in tpl:
+            x += float(ii + 1) ** 2 - float(ii + 1) ** 2
+    return pyperf.perf_counter() - t0
+
+
+def bench_builtin_or_method(loops):
+    range_it = iter(range(loops))
+    tpl = tuple(range(50))
+
+    lst = []
+    it = iter(set([2, 3, 4]))
+    t0 = pyperf.perf_counter()
+    for ii in range_it:
+        for ii in tpl:
+            lst.append
+            it.__length_hint__
+    return pyperf.perf_counter() - t0
+
+
+def bench_list_iter(loops):
+    range_it = iter(range(loops))
+
+    lst = list(range(5))
+    t0 = pyperf.perf_counter()
+    for ii in range_it:
+        x = 0
+        for ii in lst:
+            x += ii
+    return pyperf.perf_counter() - t0
+
+
+def bench_tuple_iter(loops):
+    range_it = iter(range(loops))
+
+    tpl = tuple(range(5))
+    t0 = pyperf.perf_counter()
+    for ii in range_it:
+        x = 0
+        for ii in tpl:
+            x += ii
+    return pyperf.perf_counter() - t0
+
+
+def bench_range_iter(loops):
+    range_it = iter(range(loops))
+
+    r = range(5)
+    t0 = pyperf.perf_counter()
+    for ii in range_it:
+        x = 0
+        for ii in r:
+            x += ii
+    return pyperf.perf_counter() - t0
+
+
+# %timeit bench_list(1000)
+
+if __name__ == "__main__":
+    runner = pyperf.Runner()
+    runner.bench_time_func("bench_list", bench_list)
+    runner.bench_time_func("bench_int", bench_int)
+    runner.bench_time_func("bench_float", bench_float)
+    runner.bench_time_func("bench_builtin_or_method", bench_builtin_or_method)
+    runner.bench_time_func("bench_list_iter", bench_list_iter)
+    runner.bench_time_func("bench_tuple_iter", bench_tuple_iter)
+    runner.bench_time_func("bench_range_iter", bench_range_iter)
