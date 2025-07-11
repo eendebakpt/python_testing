8991679d311d2afcebab35e0c66408823c3493b8
Pieter Eendebak <pieter.eendebak@gmail.com>
2025-07-11 20:47:33 +0200

    add bms

--- benchmarks/bm_long_hash.py
+++ benchmarks/bm_long_hash.py
@@ -7,8 +7,17 @@ import pyperf
 
 setup = """
 
+z = 2 << 30 << 30
+two_digit_ints = list(range(z, z + 2000))
+
 def lh():
-    for ii in range(2**34, 2**34 + 1000):
+    z = 2 << 30 << 30
+    for ii in range(z, z + 1000):
+        hash(ii)
+
+def long_hash_multi_digit():
+    z = 1 << 30 << 30 << 30 << 30
+    for ii in range(2**61, 2**61 + 1000):
         hash(ii)
 
 def small_longhash():
@@ -20,3 +29,5 @@ def small_longhash():
 runner = pyperf.Runner()
 runner.timeit(name="long_hash(small int)", stmt="small_longhash()", setup=setup)
 runner.timeit(name="long_hash", stmt="lh()", setup=setup)
+runner.timeit(name="long_hash_multi_digit", stmt="long_hash_multi_digit()", setup=setup)
+runner.timeit(name="set(ints)", stmt="set(two_digit_ints)", setup=setup)
